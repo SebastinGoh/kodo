e623d6f5bc85290f6d5aa91194b576610dc23033
SebastinGoh <gmcsebastin@gmail.com>
2023-09-20 16:41:45 +0800

    add another change to navbar to try to solve safari issue

--- app/logo.tsx
+++ app/logo.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link"
 
 export default function Logo() { return (
-    <Link href="/" className='flex gap-2 max-h-10'>
-        <img alt="logo" src="/logo-bare.png" className=''/>
-        <img alt="logo" src="/logo-word.png" className=''/>
+    <Link href="/" className='flex'>
+        <img alt="logo" src="/logo-bare.png" className='max-h-10'/>
+        <img alt="logo" src="/logo-word.png" className='max-h-10'/>
     </Link>
 )};
