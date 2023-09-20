8d30dd50409a85e82f392deb64b9ab224345ffdc
SebastinGoh <gmcsebastin@gmail.com>
2023-09-20 16:40:36 +0800

    add changes to navbar to try to solve menu issue in safari

--- app/logo.tsx
+++ app/logo.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link"
 
 export default function Logo() { return (
-    <Link href="/" className='flex items-center gap-2 max-h-10'>
-        <img alt="logo" src="/logo-bare.png" className='h-full'/>
-        <img alt="logo" src="/logo-word.png" className='h-full'/>
+    <Link href="/" className='flex gap-2 max-h-10'>
+        <img alt="logo" src="/logo-bare.png" className=''/>
+        <img alt="logo" src="/logo-word.png" className=''/>
     </Link>
 )};
