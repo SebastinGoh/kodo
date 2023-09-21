7c6d88c000d66ee22c308a58bd0da34c291df403
SebastinGoh <gmcsebastin@gmail.com>
2023-09-21 09:57:43 +0800

    test navbar menu icon with words and fixing autoplay on iphone

--- app/herovideo.tsx
+++ app/herovideo.tsx
@@ -4,7 +4,7 @@ export default function HeroVideo() {
     const video = "/hero_video_L.mp4"
     return (
         <div className="h-[85%] relative">
-            <video src={video} className="w-full h-full object-cover" autoPlay muted loop={true}>
+            <video src={video} className="w-full h-full object-cover" autoPlay muted loop={true} playsInline>
             </video>
             <div className="absolute left-0 top-0 w-full h-full flex flex-col justify-center items-center gap-4 text-slate-900">
                 <img alt="logo" src="/logo.png" className='lg:w-1/5 md:w-1/4 w-1/3'/>
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -32,9 +32,10 @@ export default function Navbar() {
                     </Link>
                 </div>
                 <Link href="#" className="md:hidden pl-10 cursor-pointer" onClick={toggle}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
+                    </svg> */}
+                    Menu
                 </Link>
             </nav>
         </header>
