11154c76c975bc4a8b0b85b6103ee874a98eb80c
SebastinGoh <gmcsebastin@gmail.com>
2023-09-21 10:10:30 +0800

    test navbar links on iphone

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
         <header className="bg-beige font-slate-900">
             <nav className="flex p-6 justify-between">
                 <Logo />
-                <div className='hidden md:flex h-auto gap-14 items-center'>
+                <div className='flex md:flex h-auto gap-14 items-center'>
                     <Link href="/product" className="">
                         Product
                     </Link>
@@ -31,7 +31,7 @@ export default function Navbar() {
                         </svg>
                     </Link>
                 </div>
-                <Link href="#" className="md:hidden pl-10 cursor-pointer" onClick={toggle} onTouchStart={toggle}>
+                <Link href="#" className="md:hidden pl-10 cursor-pointer" onClick={toggle}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
