af57e6df77d84a9f3f6beda96e685a185e34c620
SebastinGoh <gmcsebastin@gmail.com>
2023-09-21 10:02:36 +0800

    add onTouchStart to navbar to try to fix broken menu icon

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -31,7 +31,7 @@ export default function Navbar() {
                         </svg>
                     </Link>
                 </div>
-                <Link href="#" className="md:hidden pl-10 cursor-pointer" onClick={toggle}>
+                <Link href="#" className="md:hidden pl-10 cursor-pointer" onClick={toggle} onTouchStart={toggle}>
                     {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg> */}
