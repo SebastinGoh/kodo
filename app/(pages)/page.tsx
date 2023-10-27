b7e67ed0c2c519a7f08188f9db97a8a91da23ecc
SebastinGoh <gmcsebastin@gmail.com>
2023-10-27 16:34:53 +0800

    add small fix to payment success redirect to overlay

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -11,19 +11,19 @@ import Catalogue from "@/app/components/products/catalogue";
 import Socials from "@/app/components/home/socials";
 
 export default function Home() {
-  const urlParams = useSearchParams()
+  const urlParams = useSearchParams();
 
   const status = urlParams.get('status')?.toString();
   const openCheckout = useOverlayStore(state => state.openCheckout);
   const setPaymentSuccess = useCartStore(state => state.setPaymentSuccess);
   const totalPrice = useCartStore(state => state.totalPrice);
-
+  const isTotalPriceZero = totalPrice === 0.00;
   useEffect(() => {
-    if (status === 'completed' && totalPrice !== 0.00) {
+    if (status === 'completed' && !isTotalPriceZero) {
       setPaymentSuccess(true);
       openCheckout();
     }
-  }, [status]);
+  }, [status,totalPrice]);
 
   return (
     <main className='bg-blue max-h-fit'>
