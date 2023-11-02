3367c42fbb95e93cb4b5a08ab04ca304fc818b7e
SebastinGoh <gmcsebastin@gmail.com>
2023-11-02 12:10:30 +0800

    add email template to show formatting on email providers for contact us page

--- app/api/contact-email/email-template.ts
+++ app/api/contact-email/email-template.ts
@@ -6,115 +6,110 @@ export default function EmailTemplate({ email, name, message }: { email:string, 
         bcc: process.env.MY_EMAIL,
         subject: `Kodo - Thank you for contacting us!`,
         html: 
-        `<!DOCTYPE html>
-        <html>
-        <head>
-          <meta charset="utf-8">
-          <title>Contact Us Reply</title>
-          <style>
-            /* Define your styles here */
-            body {
-              font-family: Arial, sans-serif;
-              background-color: #D1D5DB;
-              margin: 0;
-              padding: 0;
-            }
-        
-            .container {
-              max-width: 600px;
-              margin: 0 auto;
-              padding: 20px;
-              background-color: none;
-              display: flex;
-              flex-direction: column;
-              gap: 30px;
-            }
-        
-            .header {
-              background-color: none;
-              color: #0E1729;
-              padding: 20px;
-              display: flex;
-              gap: 15px;
-              align-items: center;
-            }
-        
-            .header p {
-              font-size: 24px;
-              margin: 0;
-            }
-        
-            .content {
-              padding: 10px 50px;
-              display: flex;
-              flex-direction: column;
-              gap: 2px;
-              align-items: center;
-              justify-content: center;
-              background-color: #ffffff;
-              font-size: 16px;
-              text-align: center;
-            }
-    
-            .content-title {
-              font-size: 30px;
-            }
-        
-            .cta-button {
-              display: inline-block;
-              background-color: #ff5722;
-              color: #ffffff;
-              padding: 10px 20px;
-              text-decoration: none;
-              border-radius: 5px;
-            }
-    
-            .button {
-              text-decoration: none;
-              background-color: #f4e5ae;
-              border-radius: 25px;
-              font-weight: bold;
-              width: 12em;
-              height: 1.5rem;
-              padding: 0.5rem;
-              :hover {
-                background-color: #f999a4;
-              }
-            }
-        
-            .footer {
-              background-color: none;
-              padding: 10px;
-              text-align: center;
-            }
-          </style>
-        </head>
-        <body>
-          <div class="container">
-            <div class="header">
-              <img src="logo.png" alt="Kodo Logo" width="50" height="50" />
-              <p>Kodo Support</p>
-            </div>
-            <div class="content">
-              <p class="content-title">We got your message</p>
-              <p>Dear ${name},</p>
-              <p>Thanks for dropping us a line and we're delighted to assist you!</p>
-            </div>
-            <div class="content">
-              <p class="content-title">Here's your message</p>
-              <p class="message">${message}</p>
-              <p class="message">Our team is working diligently to address your request. You can anticipate a reply from us soon.</p>
-              <p class="message">In the meantime, don't hesitate to explore our selection of play dough. We're sure you'll discover something fantastic for your little learners.</p>
-              <p class="message">Best regards,</p>
-              <p class="message">The Kodo Team</p>
-              <a class="button" href="www.kodo.sg/products/">Explore Products</a>
-            </div>
-            <div class="footer">
-              &copy; 2023 Kodo LLP | <a href="mailto:hello@kodo.sg">hello@kodo.sg</a>
-            </div>
-          </div>
-        </body>
-        </html>
+`
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us Response</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #D1D5DB;
+            color: #0E1729;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #ffffff;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .header {
+            background-color: #f4e5ae;
+            padding: 10px;
+            border-radius: 5px 5px 0 0;
+            width: 100%;
+            font-size: 16px;
+            font-weight: bold;
+        }
+
+        .content {
+            padding: 20px;
+        }
+
+        .message {
+            margin-bottom: 20px;
+            font-size: 18px;
+        }
+
+        .cta-button {
+            background-color: #f4e5ae;
+            color: #0E1729;
+            text-decoration: none;
+            padding: 10px 20px;
+            border-radius: 5px;
+        }
+
+        .footer {
+            margin-top: 20px;
+            font-size: 14px;
+            color: #888;
+        }
+    </style>
+</head>
+
+<body>
+    <table class="container" cellpadding="0" cellspacing="0">
+        <tr>
+            <td>
+                <table class="header" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td class="logo">
+                          <img src="https://www.kodo.sg/logo.png" alt="Kodo Logo" width="50" height="50" />
+                        </td>
+                        <td>Kodo Support</td>
+                    </tr>
+                </table>
+                <table class="content" cellpadding="0" cellspacing="10">
+                    <tr>
+                        <td class="message">
+                            <p>Dear ${name},</p>
+                            <p>Here's the message we got from you:</p>
+                            <p style="font-family: Courier New">${message}</p>
+                            <p>Our team has received your message and will get back to you as soon as possible.</p>
+                            <p>In the meantime, don't hesitate to explore our selection of play dough. We're sure you'll discover something fantastic for your little learners.</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center">
+                            <a href="https://www.kodo.sg/products/" class="cta-button">Explore Our Products</a>
+                        </td>
+                    </tr>
+                </table>
+                <table class="footer" cellpadding="5" cellspacing="0">
+                    <tr>
+                        <td>
+                            <p>If you have any more questions, feel free to reply to this email or contact us at 
+                              <a href="mailto:hello@kodo.sg">hello@kodo.sg</a>.
+                            </p>
+                            <p>Best regards,<br> Kodo Team</p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
 `
     }
     );
